f9739ecbc94efb590f8aba446355a0f74468c551
sheerfish999 <sheerfish999@163.com>
2016-12-25 17:55:28 +0800

    新文件：   showlocal.py 	新文件：   tolink.sh 	新文件：   toport.sh

---
+++ tolink.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+
+##### args nums
+if [ $# -lt 2 ]; then
+	echo Parameter error:
+	echo Link to each other: tolink.sh CONTAINER_ID/NAME1 CONTAINER_ID/NAME2
+	exit
+fi 
+
+##### args is right
+ret=`docker ps | awk '{print($1" "$NF)}' | grep -v 'CONTAINER NAMES' | grep -w $1`
+
+# CONTAINER exist
+if [ ${#ret} -eq 0 ]; then
+	echo Parameter error:
+	echo CONTAINER_ID/NAME1: $1 is not Up_status, or no such CONTAINER_ID/NAME
+	exit
+fi 
+
+# CONTAINER exist
+ret=`docker ps | awk '{print($1" "$NF)}' | grep -v 'CONTAINER NAMES' | grep -w $2`
+
+if [ ${#ret} -eq 0 ]; then
+	echo Parameter error:
+	echo CONTAINER_ID/NAME2: $2 is not Up_status, or no such CONTAINER_ID/NAME
+	exit
+fi 
+
+
+####
+
+
+ip1=`docker inspect --format='{{.NetworkSettings.IPAddress}}' $1`
+ip2=`docker inspect --format='{{.NetworkSettings.IPAddress}}' $2`
+
+echo $1: $ip1
+echo $2: $ip2
+
+
+###  1 to 2
+
+docker exec $1 cp /etc/hosts /etc/hosts.temp
+docker exec $1 sed -i '/'$2'/d'  /etc/hosts.temp
+docker exec $1 bash -c 'cat /etc/hosts.temp > /etc/hosts'
+docker exec $1 bash -c 'echo '$ip2' '$2' >> /etc/hosts'
+
+echo $1 to $2 is OK
+
+###  2 to 1
+
+docker exec $2 cp /etc/hosts /etc/hosts.temp
+docker exec $2 sed -i '/'$1'/d'  /etc/hosts.temp
+docker exec $2 bash -c 'cat /etc/hosts.temp > /etc/hosts'
+docker exec $2 bash -c 'echo '$ip1' '$1' >> /etc/hosts'
+
+echo $2 to $1 is OK
+
+
+
+
+
+
+
+
